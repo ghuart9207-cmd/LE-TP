5628212ce39d81306acc75257aed5f66a9302059
geoffroy <geoffroy.huart@ynov.com>
2025-09-23 13:05:18 +0200

    Feat : Ajout Structures Voitures et objets et objets du coffre

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+type Voiture struct {
+	Marque    string
+	nom       string
+	Puissance int
+	Couleur   string
+	coffre    []objet
+}
+
+type TrunkItem struct {
+    name     string
+    quantity int
+}
+
+type objet struct {
+	nom      string
+	quantité int
+}
+
+func main() {
+	EVANESTENGALERE := z01.Println(BIGEVAN-Z1.HUGO
+}
